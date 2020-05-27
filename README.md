e10399ffa171cc4b21315bd7e900b572fcee2130
Martin Schuster <schuster@isja.org>
2020-05-27 20:49:18 +0200

    first working version

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ A GPS-Tracker based on Arduino and Adafruit FONA module.
 
 ## SIM5320
 
+Do not end commands with `NL CR`, only `CR`!
+
 ### Useful AT commands
 
 ```
@@ -15,17 +17,36 @@ AT+CGPSAUTO=1
 ```
 
 ```
-AT+CGATT=1
-AT+CGDCONT=1,"IP","web","0.0.0.0"
-AT+CGACT=1,1
-AT+CGPADDR=1
+AT+CVHU=0
+ATI
+AT+GSN
+AT+CFUN=1
+
 AT+CPSI?
-AT+NETOPEN
-AT+IPADDR
+AT+COPS?
+
+AT+CGATT=1
+AT+CGSOCKCONT=1,"IP","web"
+AT+CIPMODE=1
+AT+NETOPEN=,,1
+
+AT+CHTTPSSTART
+AT+CHTTPSOPSE="www.dweet.io",443,2
+AT+CHTTPSSEND=81
+GET /dweet/for/867057032632194?temp=761.37&batt=3700 HTTP/1.1
+Host: dweet.io
+
+AT+CHTTPSSEND
+AT+CHTTPSRECV?
+AT+CHTTPSRECV=0
+AT+CHTTPSCLSE
+AT+CHTTPSSTOP
 ```
 
 ### Links
 
 * [TinyGSM](https://github.com/vshymanskyy/TinyGSM) library
 * [Adafruit FONA downloads](https://learn.adafruit.com/adafruit-fona-3g-cellular-gps-breakout/downloads)
+* [SIM5320 Downloads](https://simcom.ee/documents/?dir=SIM5320)
+* [Working Arduino library](https://github.com/botletics/SIM7000-LTE-Shield)
 
--- Tracker.ino
+++ Tracker.ino
@@ -1,23 +1,33 @@
-#include <Adafruit_FONA.h>
+#include "Adafruit_FONA.h"
 #include <SoftwareSerial.h>
+
+#define SIMCOM_3G
 
 #define FONA_RX 2
 #define FONA_TX 3
 #define FONA_RST 4
+#define FONA_PWRKEY 5
 
 SoftwareSerial fonaSS(FONA_TX, FONA_RX);
-SoftwareSerial *fonaSerial = &fonaSS;
 
-Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
 
+char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
 
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    fonaSerial->begin(4800);
+    pinMode(FONA_RST, OUTPUT);
+    digitalWrite(FONA_RST, HIGH); // Default state
 
-    if (! fona.begin(*fonaSerial)) {
+    pinMode(FONA_PWRKEY, OUTPUT);
+    digitalWrite(FONA_PWRKEY, LOW);
+    delay(2000);
+    digitalWrite(FONA_PWRKEY, HIGH);
+
+    Serial.begin(115200);
+    fonaSS.begin(4800);
+
+    if (! fona.begin(fonaSS)) {
         Serial.println(F("Couldn't find FONA"));
         while(1);
     }
@@ -25,12 +35,29 @@ void setup() {
 
     Serial.println((String)"Type is " + fona.type());
 
+    uint8_t imeiLen = fona.getIMEI(imei);
+    if (imeiLen > 0) {
+        Serial.print("Module IMEI: "); Serial.println(imei);
+    }
+
     Serial.println(F("Enabling GPS..."));
     fona.enableGPS(true);
+
+    // Set modem to full functionality
+    fona.setFunctionality(1); // AT+CFUN=1
+
+    fona.setNetworkSettings(F("web")); // For Telering
+
+    // wait a little bit for connection
+    delay(1000);
+
+    fona.enableGPRS(true);
 }
 
 void loop() {
-    delay(2000);
+    delay(5000);
+
+    // fona.getNetworkInfo();
 
     float latitude, longitude, speed_kph, heading, altitude;
 
@@ -39,19 +66,40 @@ void loop() {
 
     if (gps_success) {
 
-        Serial.print("GPS lat:");
-        Serial.println(latitude, 6);
-        Serial.print("GPS long:");
-        Serial.println(longitude, 6);
-        Serial.print("GPS speed KPH:");
-        Serial.println(speed_kph);
-        Serial.print("GPS heading:");
-        Serial.println(heading);
-        Serial.print("GPS altitude:");
-        Serial.println(altitude);
+        // Serial.print("GPS lat:");
+        // Serial.println(latitude, 6);
+        // Serial.print("GPS long:");
+        // Serial.println(longitude, 6);
+        // Serial.print("GPS speed KPH:");
+        // Serial.println(speed_kph);
+        // Serial.print("GPS heading:");
+        // Serial.println(heading);
+        // Serial.print("GPS altitude:");
+        // Serial.println(altitude);
 
     } else {
         Serial.println("Waiting for FONA GPS 3D fix...");
+        return;
     }
+
+
+    // Create char buffers for the floating point numbers for sprintf
+    // Make sure these buffers are long enough for your request URL
+    char URL[150];
+    char latBuff[16];
+    char lonBuff[16];
+
+    // Format the floating point numbers as needed
+    dtostrf(latitude, 1, 3, latBuff); // float_val, min_width, digits_after_decimal, char_buffer
+    dtostrf(longitude, 1, 3, lonBuff);
+
+    // Construct the appropriate URL's and body, depending on request type
+    // Use IMEI as device ID for this example
+
+    // GET request
+    sprintf(URL, "GET /dweet/for/%s?lat=%s&lon=%s HTTP/1.1\r\nHost: dweet.io\r\n\r\n", imei, latBuff, lonBuff);
+
+    if (!fona.postData("www.dweet.io", 443, "HTTPS", URL)) // Server, port, connection type, URL
+        Serial.println(F("Failed to complete HTTP/HTTPS request..."));
 
 }
